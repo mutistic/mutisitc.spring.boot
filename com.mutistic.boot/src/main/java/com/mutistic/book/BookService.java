63d4369f4b67a9fa72ec2f4b9d680f70105fef95
mutistic-client <mutistic@qq.com>
2018-08-06 11:26:20 +0800

    提交spring boot 项目

---
+++ com.mutistic.boot/src/main/java/com/mutistic/book/BookService.java
@@ -0,0 +1,22 @@
+package com.mutistic.book;
+
+public class BookService {
+	
+}
+//import java.util.List;
+//
+//import javax.annotation.Resource;
+//
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class BookService {
+//
+//	@Resource
+//	private BookRepository bookRepository;
+//	
+////	public List<BookEntity> findAll(){
+////		return bookRepository.findAll();
+////	}
+//	
+//}
